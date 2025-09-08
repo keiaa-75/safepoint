aa87281bb0563fae832871978cc821e908b8856a
Keiaa <confinedrose@duck.com>
2025-09-08 21:52:20 +0800

    Initialized Spring project.

---
+++ src/main/java/com/keiaa/voiz/VoizApplication.java
@@ -0,0 +1,13 @@
+package com.keiaa.voiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keiaa/voiz/VoizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keiaa.voiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
