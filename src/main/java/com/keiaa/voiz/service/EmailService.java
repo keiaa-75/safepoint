f0a2b0aeda013d1ba20cfc971a509d08482bee06
Keiaa <confinedrose@duck.com>
2025-09-09 14:35:00 +0800

    Apply MPL 2.0 to new files.

--- src/main/java/com/keiaa/voiz/service/EmailService.java
+++ src/main/java/com/keiaa/voiz/service/EmailService.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://www.mozilla.org/MPL/2.0/.
+ */
+
 package com.keiaa.voiz.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +22,10 @@ public class EmailService {
         message.setFrom("your-email@gmail.com"); // Your configured email address
         message.setTo(toEmail);
         message.setSubject("SafePoint: Report Submitted Successfully");
-        message.setText("Thank you for submitting your report. Your unique tracking ID is: " + reportId + 
-                        ". Please save this ID to check the status of your report at a later time.");
+        message.setText(
+            "Thank you for submitting your report. Your unique tracking ID is: " + reportId + 
+            ". Please save this ID to check the status of your report at a later time."
+            );
         mailSender.send(message);
     }
 }
