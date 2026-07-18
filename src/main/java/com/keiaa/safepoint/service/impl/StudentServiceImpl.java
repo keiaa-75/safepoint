a329479ad323d293cf4d598139973d0f0ba201dc
Keiaa <confinedrose@duck.com>
2026-07-18 18:41:26 +0800

    feat (auth): pre-check unique fields with clear exs

--- src/main/java/com/keiaa/safepoint/service/impl/StudentServiceImpl.java
+++ src/main/java/com/keiaa/safepoint/service/impl/StudentServiceImpl.java
@@ -1,17 +1,14 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://www.mozilla.org/MPL/2.0/.
- */
-
 package com.keiaa.safepoint.service.impl;
 
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.keiaa.safepoint.exception.DuplicateEmailException;
+import com.keiaa.safepoint.exception.DuplicateLrnException;
 import com.keiaa.safepoint.model.Student;
 import com.keiaa.safepoint.repository.StudentRepository;
 import com.keiaa.safepoint.service.StudentService;
@@ -27,8 +24,22 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student registerStudent(Student student) {
+        if (studentRepository.findByEmail(student.getEmail()).isPresent()) {
+            throw new DuplicateEmailException("An account with this email address already exists.");
+        }
+        if (studentRepository.findByLrn(student.getLrn()).isPresent()) {
+            throw new DuplicateLrnException("An account with this LRN already exists.");
+        }
+
         student.setPassword(passwordEncoder.encode(student.getPassword()));
-        return studentRepository.save(student);
+
+        try {
+            return studentRepository.save(student);
+        } catch (DataIntegrityViolationException e) {
+            // Safety net if two signups for the same email/LRN land within
+            // milliseconds of each other, past the checks above.
+            throw new DuplicateEmailException("An account with this email or LRN already exists.");
+        }
     }
 
     @Override
